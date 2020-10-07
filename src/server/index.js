1ce66732633aee528bb57d3d76c4947e7aaab023
Davidvision <hklein@student.42.fr>
2020-10-07 17:01:26 +0200

    removed mongodb support, user model and user router

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-require("./db/mongoose");
-
-const userRouter = require("./routers/user");
 
 const { port, host } = require("../../params").server;
 const app = express();
@@ -10,8 +7,8 @@ const app = express();
 app.use(bodyParser.json({ limit: "200mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "200mb", extended: true }));
 
-app.use(userRouter);
-
-app.listen(port, host, () => {
+let server = app.listen(port, host, () => {
   console.log(`Server is up on port ${port}`);
 });
+
+module.exports = server;
--- test/server1.js
+++ test/server1.js
@@ -1,38 +1,38 @@
-// import chai from "chai";
-// import { startServer, configureStore } from "./helpers/server";
-// import rootReducer from "../src/client/reducers";
-// import { ping } from "../src/client/actions/server";
-// import io from "socket.io-client";
-// import params from "../params";
+import chai from "chai";
+import { startServer, configureStore } from "./helpers/server";
+import rootReducer from "../src/client/reducers";
+import { ping } from "../src/client/actions/server";
+import io from "socket.io-client";
+import params from "../params";
 
-// const chai = require("chai");
-// const { startServer, configureStore } = require("./helpers/server");
-// const rootReducer = require("../src/client/reducers");
-// const { ping } = require("../src/client/actions/server");
-// const io = require("socket.io-client");
-// const params = require("../params");
+const chai = require("chai");
+const { startServer, configureStore } = require("./helpers/server");
+const rootReducer = require("../src/client/reducers");
+const { ping } = require("../src/client/actions/server");
+const io = require("socket.io-client");
+const params = require("../params");
 
-// chai.should();
+chai.should();
 
-// describe("Fake server test", function() {
-//   let tetrisServer;
-//   before(cb =>
-//     startServer(params.server, function(err, server) {
-//       tetrisServer = server;
-//       cb();
-//     })
-//   );
+describe("Fake server test", function() {
+  let tetrisServer;
+  before(cb =>
+    startServer(params.server, function(err, server) {
+      tetrisServer = server;
+      cb();
+    })
+  );
 
-//   after(function(done) {
-//     tetrisServer.stop(done);
-//   });
+  after(function(done) {
+    tetrisServer.stop(done);
+  });
 
-//   it("should pong", function(done) {
-//     const initialState = {};
-//     const socket = io(params.server.url);
-//     const store = configureStore(rootReducer, socket, initialState, {
-//       pong: () => done()
-//     });
-//     store.dispatch(ping());
-//   });
-// });
+  it("should pong", function(done) {
+    const initialState = {};
+    const socket = io(params.server.url);
+    const store = configureStore(rootReducer, socket, initialState, {
+      pong: () => done()
+    });
+    store.dispatch(ping());
+  });
+});
